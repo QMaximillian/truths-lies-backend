f3e8a36f6186015a77a23357aece9c1013bae1c1
QMaximillian <quinnlashinsky@gmail.com>
2018-07-31 17:05:05 -0400

    Quiz card creation

--- rails-backend/app/controllers/api/v1/quiz_cards_controller.rb
+++ rails-backend/app/controllers/api/v1/quiz_cards_controller.rb
@@ -1,10 +1,21 @@
-module Api 
+module Api
 	module V1
 		class QuizCardsController < ApplicationController
-			def index 
-				@quizcards = QuizCard.all 
+			def index
+				@quizcards = QuizCard.all
 				render json: @quizcards
 			end
+
+			def create
+				@quizcard = QuizCard.create(quiz_card_params)
+				render json: @quizcard
+			end
+
+			private
+
+			def quiz_card_params
+				params.require(:quiz_card).permit(:player_id, :option1, :option2, :option3, :false_option)
+			end
 		end
-	end 
+	end
 end
