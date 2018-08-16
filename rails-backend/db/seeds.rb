cfc0b368891aab49eb73911feab5eacff9f571d4
QMaximillian <quinnlashinsky@gmail.com>
2018-08-16 17:13:01 -0400

    Commit

--- rails-backend/db/seeds.rb
+++ rails-backend/db/seeds.rb
@@ -20,12 +20,12 @@ Player.create(name: "Mally Malz", high_score: 0)
 Player.create(name: "Bruce Wayne (Roniece)", high_score: 0)
 Player.create(name: "Angelina", high_score: 0)
 Player.create(name: "Eric R.", high_score: 0)
-
+Player.create(name: "Mark U.", high_score: 0)
 
 QuizCard.create(player_id: 1, option1: "Has been outside of the US.", option2: "Has never broken a bone.", option3: "Studied Musical Theater", false_option: 1)
 QuizCard.create(player_id: 2, option1: "Has been working since 16.", option2: "Likes lizards.", option3: "Doesn't use the restroom.", false_option: 3)
 QuizCard.create(player_id: 3, option1: "Has been trapped in gondola.", option2: "Has 2 cats.", option3: "Has never been to Chicago.", false_option: 2)
-QuizCard.create(player_id: 4, option1: "Is a latina.", option2: "Has licked a donut.", option3: "Hates America.", false_option: 1)
+QuizCard.create(player_id: 4, option1: "Is Latina.", option2: "Has licked a donut.", option3: "Has won ten Teen Choice awards.", false_option: 1)
 QuizCard.create(player_id: 5, option1: "From Maplewood, New Jersey.", option2: "Voice is permanently damaged.", option3: "Is a Scorpio.", false_option: 2)
 QuizCard.create(player_id: 6, option1: "Has 3 daughters", option2: "First black president of the Harvard Law Review", option3: "Is a wind surfer", false_option: 1)
 QuizCard.create(player_id: 7, option1: "Only wears black", option2: "Straight Edge", option3: "Introduced on Beavis & Butthead", false_option: 1)
@@ -36,5 +36,5 @@ QuizCard.create(player_id: 11, option1: "Can sing in Russian", option2: "Has liv
 QuizCard.create(player_id: 12, option1: "Sounds like a rat when startled", option2: "Once accepted a ride from a strange man", option3: "Allergic to apples", false_option: 3)
 QuizCard.create(player_id: 13, option1: "Has only gone abroad to asian countries", option2: "prefers cold pizza", option3: "used to be an emo kid", false_option: 2)
 QuizCard.create(player_id: 14, option1: "hacker", option2: "has been out the country", option3: "likes to swim", false_option: 3)
-
+QuizCard.create(player_id: 15, option1: "I helped my friend write a song for a famous pop star", option2: "My ancestors attempted to assassinate George Washington", option3: "My bands only have followings in Eastern Europe and Montreal", false_option: 1)
 
